d88131f3c19ab35bba22fa7e727f9adb267ee8b2
farzam khoddam <farzamfara85@gmail.com>
2024-02-03 14:56:32 +0330

    improvement

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fda11f75'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gvfot67o81o"
+    "revision": "0.5gb00i6h3ng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
